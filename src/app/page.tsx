099c6d4190a26689df2ee4245a3066b6f03803a4
gummipainkillers <zactaylorwestermann@gmail.com>
2025-02-17 23:15:17 +1100

    auth & database setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1>home page code</h1>
+    </div>
+  );
+}
---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,11 @@
+"use client"; //this makes it a client component rather than server component
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
