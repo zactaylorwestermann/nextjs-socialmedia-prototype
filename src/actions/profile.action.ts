778b8bee9154650e940c69d496772a8a1f3ea129
gummipainkillers <zactaylorwestermann@gmail.com>
2025-02-19 23:04:32 +1100

    error fixing - ready to deploy

--- src/actions/profile.action.ts
+++ src/actions/profile.action.ts
@@ -1,4 +1,4 @@
-"user server";
+"use server";
 
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -8,6 +8,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -55,6 +56,19 @@ function CreatePost() {
           </div>
 
           {/*Image Uploads */}
+
+          {(showImageUpload || imageUrl) && (
+            <div className="border rounded-lg p-4">
+              <ImageUpload
+                endpoint="postImage"
+                value={imageUrl}
+                onChange={(url) => {
+                  setImageUrl(url);
+                  if (!url) setShowImageUpload(false);
+                }}
+              />
+            </div>
+          )}
 
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
